18399ec69b1c6391f8fc84129abd9d06c0577599
Gregor <gregor@martynus.net>
2018-11-20 13:56:42 -0800

    feat: initial version

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = octokitRenameBranch
+
+const renameBranch = require('./lib/rename-branch')
+
+function octokitRenameBranch (octokit) {
+  octokit.renameBranch = renameBranch.bind(null, octokit)
+}
