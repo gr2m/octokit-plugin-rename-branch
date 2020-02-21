fc1d95b56e92b86639e30d2c3790eaf0c064f476
Gregor Martynus <gregor@martynus.net>
2020-02-20 16:11:56 -0800

    test: replace `@octokit/rest` with `@octokit/core` and `@octokit/plugin-paginate-rest`

--- test/happy-path-test.js
+++ test/happy-path-test.js
@@ -1,36 +1,50 @@
-const { test } = require('tap')
+const { test } = require("tap");
 
-const Octokit = require('@octokit/rest')
-  .plugin(require('..'))
+const { Octokit: Core } = require("@octokit/core");
+const { paginateRest } = require("@octokit/plugin-paginate-rest");
+const renameBranch = require("..");
+const Octokit = Core.plugin([paginateRest, renameBranch]);
 
-test('happy path', async t => {
-  const fixtures = require('./fixtures/happy-path')
-  const octokit = new Octokit()
+test("happy path", async t => {
+  const fixtures = require("./fixtures/happy-path");
+  const octokit = new Octokit();
 
-  octokit.hook.wrap('request', (_, options) => {
-    const currentFixtures = fixtures.shift()
-    const { baseUrl, method, url, request, headers, mediaType, ...params } = options
+  octokit.hook.wrap("request", (_, options) => {
+    const currentFixtures = fixtures.shift();
+    const {
+      baseUrl,
+      method,
+      url,
+      request,
+      headers,
+      mediaType,
+      ...params
+    } = options;
 
     if (currentFixtures.request.url !== options.url) {
-      debugger
+      debugger;
     }
 
-    t.equal(currentFixtures.request.method, options.method, 'method matches')
-    t.equal(currentFixtures.request.url, options.url, 'URL matches')
+    t.equal(currentFixtures.request.method, options.method, "method matches");
+    t.equal(currentFixtures.request.url, options.url, "URL matches");
 
     Object.keys(params).forEach(paramName => {
-      t.equal(currentFixtures.request[paramName], params[paramName], 'param matches')
-    })
+      t.deepequal(
+        currentFixtures.request[paramName],
+        params[paramName],
+        "param matches"
+      );
+    });
 
-    return currentFixtures.response
-  })
+    return currentFixtures.response;
+  });
 
   await octokit.renameBranch({
-    owner: 'gr2m',
-    repo: 'rename-branch-test',
-    current_name: 'master',
-    name: 'latest'
-  })
+    owner: "gr2m",
+    repo: "rename-branch-test",
+    current_name: "master",
+    name: "latest"
+  });
 
-  t.equal(fixtures.length, 0)
-})
+  t.equal(fixtures.length, 0);
+});
